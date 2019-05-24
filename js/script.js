2521b50ec1e7782238672ebfa85f097806735734
flaviadessoldi <flavia.dessoldi@gmail.com>
2019-05-23 22:25:15 -0300

    deletar mulher da API

--- js/script.js
+++ js/script.js
@@ -2,49 +2,80 @@ let box = document.querySelector('.maravilhosas__box');
 
 fetch('http://localhost:5001/maravilhosas')
 
-.then((response)=>{
-    return response.json();
-})
+    .then((response) => {
+        return response.json();
+    })
 
-.then((data)=>{
-    console.log('Deu certo!');
-    console.log(data)
-    
-    data.content.forEach((mulheres)=>{
+    .then((data) => {
+        console.log('Deu certo!');
+        console.log(data)
 
-        console.log(mulheres, 'mulheres dentro do foreach')
-        
-        let perfilMulher = document.createElement('div');
-        perfilMulher.setAttribute('class', 'maravilhosas__perfil');
-        box.appendChild(perfilMulher);
-        
-        let linkMulher = document.createElement('a');
-        linkMulher.setAttribute('href','#');
-        perfilMulher.appendChild(linkMulher);
+        data.content.forEach((mulheres) => {
 
-        let foto = document.createElement('img');
-        foto.setAttribute('class', 'img-responsive');
-        if (mulheres.metadata && mulheres.metadata.image) {
-            foto.setAttribute('src', mulheres.metadata.image.url);
-                } else {
+            console.log(mulheres, 'mulheres dentro do foreach')
+
+            let perfilMulher = document.createElement('div');
+            perfilMulher.setAttribute('class', 'maravilhosas__perfil');
+            box.appendChild(perfilMulher);
+
+            let linkMulher = document.createElement('a');
+            linkMulher.setAttribute('href', '#');
+            perfilMulher.appendChild(linkMulher);
+
+            let foto = document.createElement('img');
+            foto.setAttribute('class', 'img-responsive');
+            if (mulheres.metadata && mulheres.metadata.image) {
+                foto.setAttribute('src', mulheres.metadata.image.url);
+            } else {
                 foto.setAttribute('src', './img/img-mulher.png');
             }
-       
-                 
-        linkMulher.appendChild(foto);
 
-        let nomeMulher = document.createElement('p');
-        nomeMulher.textContent= mulheres.title;
-        
-        linkMulher.appendChild(nomeMulher);
-        
-    })
-    
+
+            linkMulher.appendChild(foto);
+
+            let nomeMulher = document.createElement('p');
+            nomeMulher.textContent = mulheres.title;
+
+            linkMulher.appendChild(nomeMulher);
+
+            const button = document.createElement('button');
+            button.setAttribute('data-id', mulheres.id);
+            button.textContent = '✖';
+            perfilMulher.appendChild(button)
+
+            button.addEventListener('click', () => {
+                perfilMulher = button.parentElement;
+                linkMulher = perfilMulher.parentElement;
+
+                fetch('https://reqres.in/api/users', {
+                    method: 'DELETE',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+
+                    body: JSON.stringify({
+                        'id': button.getAttribute('data-id')
+                    })
+
+                })
+
+                    .then(() => {
+                        perfilMulher.removeChild(linkMulher)
+
+                    })
+                    .catch((erro) => {
+                        console.log(erro)
+
+                    })
+            })
+        })
+
     })
 
-.catch((erro)=>{
-    console.log(erro)
-})
+    .catch((erro) => {
+        console.log(erro)
+    })
 
 
 const button = document.getElementById('button');
@@ -53,7 +84,7 @@ button.addEventListener('click', (submeter) => {
 
     const nome = document.getElementById('nome').value;
     const imagem = document.getElementById('imagem').value;
-   
+
 
     fetch('http://localhost:5001/maravilhosas', {
         method: 'POST',
@@ -63,26 +94,26 @@ button.addEventListener('click', (submeter) => {
         },
         body: JSON.stringify({
             'title': nome,
-            'metadata':{
-                'image':{
-                    'url':imagem,
+            'metadata': {
+                'image': {
+                    'url': imagem,
                 }
             }
         })
-        
+
     })
         .then((response) => {
             return response.json();
         })
         .then((data) => {
             console.log(data);
-            
+
         })
 
         .catch((erro) => {
             console.log(erro);
         })
 
-        location.reload(true)
+    location.reload(true)
 })
 
